3cb04e6c7c0895b981f937879ce46d4ed3a1202b
jiayinldf <jiayinldf@JIAYINLY>
2023-04-19 14:06:48 +1200

    添加项目文件。

---
+++ 第一章例题.c
@@ -0,0 +1,47 @@
+/*Àı1.1
+#include<stdio.h>
+int main()
+{
+	printf("This is a program.\n");
+	return 0;
+}
+*/
+
+/*Àı1.2
+#include<stdio.h>
+int main()
+{
+	int a, b, sum;
+	a = 123;
+	b = 456;
+	sum = a + b;
+	printf("sum is %d", sum);
+	return 0;
+}
+*/
+
+/*Àı1.3
+#include<stdio.h>
+int main()
+{
+	int max(int x, int y);
+	int a, b, c;
+	printf("ÏÈÊäÈëa:\n");
+	scanf_s("%d", &a);
+	printf("enter b:\n");
+	scanf_s("%d", &b);
+	c = max(a, b);
+	printf("max=%d", c);
+	return 0;
+}
+
+int max(int x, int y)
+{
+	int z;
+	if (x > y)z = x;
+	else z = y;
+	return z;
+}
+*/
+
+//
---
+++ 第二章例题.c
@@ -0,0 +1,20 @@
+/*例2.18 求5！
+#include<stdio.h>
+int main()
+{
+	int t, i;
+	t = 1;
+	i = 1;
+	while (i<=5)
+	{
+		t = t * i;
+		printf("第%d循环,t=%d\n", i, t);
+		i = i + 1;
+	}
+	printf("%d个循环结束，t=%d\n", i-1, t);
+	return 0;
+
+}
+*/
+
+//例2.19 求多项式的值

7c27459fc75a9b8df1ea14f0f30f619743124770
jiayinldf <jiayinly@126.com>
2023-04-29 19:58:49 +1200

    例题 5.10

--- 第五章例题.c
+++ 第五章例题.c
@@ -260,7 +260,203 @@ int main()
 
 
 
+/*例5.7 计算pi的值
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int sig=1,n=1;
+	double pi,a=1.0,sum=0;
+	
+	while (fabs(a)>=1e-8)
+	{
 
+		a = sig * (1.0 / ((2 * n)-1));
+		sum = sum + a;
+		sig = -sig;
+		n++;
+	}
+	pi = 4 * sum;
+	printf("pi=%10.8f	n=%d", pi, n);
+	return 0;
+}
+*/
+
+//例5.8 求Fibonacci(斐波那契)数列的前40个数
+// 方法一：自己写的方法
+/*
+#include<stdio.h>
+
+int main()
+{
+	int i = 3,sum1=1,sum2=1,sum3;
+	printf("%12d%12d", sum1, sum2);
+	for (; i <= 40; i++)
+	{
+		
+		sum3 = sum2 + sum1;
+		sum1 = sum2;
+		sum2 = sum3;
+		
+		printf("%12d", sum3);
+		if (i % 4 == 0) printf("\n");
+		
+	}
+	return 0;
+
+	
+}
+*/
+
+//方法二：课本上的方法，更简洁
+/*
+#include<stdio.h>
+int main()
+{
+	int i, sum1 = 1, sum2 = 1, sum3 = 2, sum4 = 3;
+	
+	for (i=1; i <= 10; i++)
+	{
+		printf("%12d%12d%12d%12d", sum1, sum2,sum3,sum4);
+
+		sum1 = sum3 + sum4;
+		sum2 = sum1 + sum4;
+		sum3 = sum2 + sum1;
+		sum4 = sum2 + sum3;
+			
+		printf("\n");
+	}
+	return 0;
+}
+*/
+
+
+//例5.9 输入一个大于3的整数n，判定其是否为素数
+/*#include<stdio.h>
+int main()
+{
+	int i,n,a;
+	printf("please enter a number\n");
+	scanf_s("%d", &n);
+
+	for ( i = 2; i <= n-1; i++)
+	{
+		if (n % i == 0)
+		{
+				a = 1;
+				break;
+		}
+		else
+		{
+			a = 2;
+		}
+	}
+	if (a == 1)
+	{
+		printf("%d 不是素数\n", n);
+	}
+	else
+	{
+		printf("%d 是素数\n", n);
+	}
+	return 0;
+}
+*/
+
+/*例5.10 求100 - 200内的全部素数
+#include<stdio.h>
+int main()
+{
+	int i, n, a,m=0;
+	for (n = 100; n <= 200; n++)
+	{
+		for (i = 2; i <=n-1; i++)
+		{
+			
+			if (n % i == 0)
+			{
+				a = 1;
+				break;
+			}
+			else
+			{
+				a = 2;
+			}
+
+				
+		}
+		if (a == 1)
+		{
+			//printf("%d 不是素数\n", n);
+		}
+		else
+		{
+			printf("%d	", n);
+			m = m + 1;//m用来控制换行，当累计10个数时，换行。
+		}
+		if (m % 10 == 0) printf("\n");
+		
+	}
+	return 0;
+}
+
+*/
+
+//例5.11 译密码
+/*#include<stdio.h>
+int main() 
+{
+	char c;
+	c = getchar();
+	while (c!='1')
+	{
+		if ((c>='a'&&c<='z')||(c>='A'&&c<='Z'))
+		{
+			if ((c >= 'w' && c <= 'z') || (c >= 'W' && c <= 'Z'))
+			{
+				c = c + 4 - 26;
+			}
+			else
+				c = c + 4;
+			
+		}
+
+		printf("%c", c);
+		c = getchar();
+	}
+	printf("ok\n");
+	return 0;
+
+}
+*/
+
+//方法二
+/*#include<stdio.h>
+int main()
+{
+	char c;
+	
+	while ((c=getchar()) != '1')
+	{
+		if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		{
+			if ((c >= 'w' && c <= 'z') || (c >= 'W' && c <= 'Z'))
+			{
+				c = c + 4 - 26;
+			}
+			else
+				c = c + 4;
+
+		}
+
+		printf("%c", c);
+		
+	}
+	printf("ok\n");
+	return 0;
+
+}
+*/
 
 
 

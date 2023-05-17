775c3f019bd69c2d6663b663b9d5505ef5b10e57
jiayinldf <jiayinly@126.com>
2023-05-18 10:25:37 +1200

    11

--- 第8章例题.c
+++ 第8章例题.c
@@ -458,22 +458,23 @@ int main()
 */
 
 //例8.13 输出二维数组任一行任一列元素的值(使用指针方法)
+//方法一：
 #include<stdio.h>
 int main()
 {
 	int a[3][4] = { 1,3,5,7,9,11,13,15,17,19,21,23 };
 	int i, j, * p;
-	p = a[0];
-	for ( ; p < a[0] + 12; p++)
+	p = a;
+	for ( ; p < a + 12; p++)
 	{
-		if ((p-a[0]) % 4 == 0)
+		if ((p-a) % 4 == 0)
 		{
 			puts("");
 		}
 		printf("%4d", *p);
 
 	}
-	puts("");
+	puts("\n");
 	printf("enter row\n");
 	scanf_s("%d",&i);
 	puts("enter col\n");
@@ -481,3 +482,4 @@ int main()
 	p = a;
 	printf("%d", *(p+(4 * i + j)));
 }
+//方法二：

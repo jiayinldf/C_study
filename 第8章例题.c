675d52da7f95b53129848636e7aadbc594ea4e42
jiayinldf <jiayinly@126.com>
2023-05-22 09:59:11 +1200

    11

--- 第8章例题.c
+++ 第8章例题.c
@@ -563,7 +563,12 @@ void search(float(*p)[4], int n)
 #include<stdio.h>
 int main()
 {
-
+	void average(float* p, int n);
+	void search(float(*p)[4], int n);
+	float score[3][4] = { {65,67,70,60},{80,87,90,81},{90,99,100,98} };
+	average(*score, 12);
+	search(score, 2);
+	return 0;
 }
 
 void average(float* p, int n)
@@ -575,18 +580,21 @@ void average(float* p, int n)
 		sum = sum + *p;
 	}
 	average = sum / n;
-	printf("the average=%5.2f", average);
+	printf("the average=%5.2f\n", average);
+	
 	
 }
 
 
 void search(float(*p)[4], int n)
 {
-	float;
+	
+	int i;
 	printf("the score of No.%d are:\n", n);
-	for ( i = 0; i < length; i++)
+	for ( i = 0; i < 4; i++)
 	{
-
+		printf("%5.2f ", *(*(p + n) + i));
+		puts("");
 	}
 }
 

31bf05bf2edc29917120bd96fae0182abc8f624b
jiayinldf <jiayinly@126.com>
2023-06-07 10:30:35 +1200

    11

--- 第8章例题.c
+++ 第8章例题.c
@@ -1109,6 +1109,33 @@ int add(int x, int y)
 */
 
 
+//例题8.25 有a个学生，每个学生有b门课程的成绩。要求在用户输入学生序号以后，能输出该学生的全部成绩。用指针函数来实现。
+#include<stdio.h>
+int main()
+{
+	float score[][4] = { {60,70,80,90},{56,89,67,88},{34,78,90,66} };
+	float* search(float(*pointer)[4], int n);
+	float* p;
+	int i, k;
+	printf("enter the number of student:");
+	scanf_s("%d", &k);
+	printf("the scores of No.%d are:\n", k);
+	p = search(score, k);
+	for ( i = 0; i < 4; i++)
+	{
+		printf("%5.2f\t", *(p + i));
+	}
+	printf("\n");
+	return 0;
+}
+
+float* search(float(*pointer)[4], int n)
+{
+	float* pt;
+	pt = *(pointer + n);
+	return (pt);
+}
+
 
 
 

c9ad659197ab37eaed1333297c6c907709fe0c8d
jiayinldf <jiayinly@126.com>
2023-09-14 18:25:40 +1200

    2023.9.14

--- 第9章例题.c
+++ 第9章例题.c
@@ -340,12 +340,109 @@ int main()
 //例题9.9 建立动态链表 写一个函数建立一个有3名学生数据的单向动态链表
 
 
+//9.5 共用体类型
+/*
+union Data
+{
+	int i;
+	char ch;
+	float f;
+
+}a,b,c;
 
 
+union Data
+{
+	int i;
+	char ch;
+	float f;
+
+};
+union Data a, b, c;
+
+*/
 
 
+/*
+#include<stdio.h>
+union Data
+{
+	float i;
+	char ch;
+	float f;
+
+}a;
+int main()
+{
+	a.i = 97.0;
+	printf("%5.1f\n", a.i);
+	//union Data a = { .ch= 'j' };
+	//printf("%c\n", a.ch);
+
+	printf("%f\n", a.f);
+	return 0;
+
+}
+
+*/
 
 
+//例题9.11
+/*
+#include<stdio.h>
+struct 
+{
+	int num;
+	char name[10];
+	char sex;
+	char job;
+	union 
+	{
+		int clas;
+		char position[10];
+	}category;
+}person[2];
+
+int main()
+{
+	int i;
+	for ( i = 0; i < 2; i++)
+	{
+		printf("Please enter the data of person:\n");
+		scanf("%d %s %c %c",&person[i].num, &person[i].name, &person[i].sex, &person[i].job);
+		if (person[i].job == 's')
+		{
+			scanf("%d",&person[i].category.clas);
+
+		}
+		else if (person[i].job == 't')
+		{
+			scanf("%s",&person[i].category.position);
+		}
+		else
+			printf("Input error!");
+
+	}
+
+	printf("\n");
+	printf("No.	name	sex job class/position\n");
+	for ( i = 0; i < 2; i++)
+	{
+		if (person[i].job=='s')
+		{
+			printf("%-6d%-10s%-4c%-4c%-10d\n", person[i].num, person[i].name, person[i].sex, person[i].job, person[i].category.clas);
+		}
+		else
+		{
+			printf("%-6d%-10s%-4c%-4c%-10s\n", person[i].num, person[i].name, person[i].sex, person[i].job, person[i].category.position);
+		}
+	}
+
+	return 0;
+
+}
+
+*/
 
 
 
